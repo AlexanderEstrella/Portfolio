f2890445f13a8becef6c3e9862f33513147b78f6
AlexanderME <97694971+AlexanderEstrella@users.noreply.github.com>
2024-12-16 21:52:32 -0500

    added loader logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Sections/Navbar";
 import Home from "./Sections/Home";
 import About from "./Sections/About";
 import Projects from "./Sections/Projects";
 import Contact from "./Sections/Contact";
 import Skills from "./Sections/Skills";
+import LoadingLogo from "./Sections/loader";
 import "./App.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <LoadingLogo />;
+  }
   return (
     <>
       <Navbar />
