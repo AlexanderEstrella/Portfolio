66b1216b2d051653b806a3b35742f5d601e15457
AlexanderME <97694971+AlexanderEstrella@users.noreply.github.com>
2024-12-16 21:56:34 -0500

    removed loader

--- src/App.js
+++ src/App.js
@@ -5,21 +5,9 @@ import About from "./Sections/About";
 import Projects from "./Sections/Projects";
 import Contact from "./Sections/Contact";
 import Skills from "./Sections/Skills";
-import LoadingLogo from "./Sections/loader";
 import "./App.css";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    window.addEventListener('load', () => {
-      setIsLoading(false);
-    });
-  }, []);
-
-  if (isLoading) {
-    return <LoadingLogo />;
-  }
   return (
     <>
       <Navbar />
